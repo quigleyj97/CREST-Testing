526dc82b20519a2c4143edb0c2009e9006877fc1
Joe Quigley <quigleyj@mssm.org>
2016-06-29 07:28:02 -0400

    Small correction from earlier

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,8 @@
 var CREST = {
     client_id:"f8f3a75e20b94a25b9c1634aa382a63c",
-    redirect_uri:"https://quigleyj97.github.io/CREST-Testing/",
+    redirect_uri:encodeURIComponent("https://quigleyj97.github.io/CREST-Testing/"),
     response_type:"token",
-    auth_endpoint:"https://login.eveonline.com/oauth/authorize",
+    auth_endpoint:"https://login.eveonline.com/oauth/authorize/",
     ssoRedirect: function ()    {
         var requestString = this.auth_endpoint +
             "?response_type=" + this.response_type +
