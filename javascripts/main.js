82e0d23e866c4c9698c6298676bbb6738668934e
Joe Quigley <quigleyj@mssm.org>
2016-06-29 07:19:23 -0400

    CREST Test
    added SSO redirect button, CREST and CRESTapp objects

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
-console.log('This would be the main JS file.');
+var CREST = {
+    client_id:"f8f3a75e20b94a25b9c1634aa382a63c",
+    redirect_uri:"https://quigleyj97.github.io/CREST-Testing/",
+    response_type:"token",
+    auth_endpoint:"https://login.eveonline.com/oauth/authorize",
+    ssoRedirect: function ()    {
+        var requestString = this.auth_endpoint +
+            "?response_type=" + this.response_type +
+            "&redirect_uri="  + this.redirect_uri +
+            "&state=" + CRESTapp.state +
+            "&scope=" + "characterSkillsRead";
+
+        d3.select("#sso_btn").attr("disabled", "disabled");
+
+        window.location = requestString;
+    }
+};
+
+var CRESTapp    =   {
+    state : "init",
+};
